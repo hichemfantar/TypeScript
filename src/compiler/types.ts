f169fbdde9eadbdd70310b189e140a6bf95d3e74
Sheetal Nandi <shkamat@microsoft.com>
2024-05-21 11:33:00 -0700

    Write buildInfo irrespective of incremental if invoked from tsc --b

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -7374,6 +7374,7 @@ export interface CompilerOptions {
     esModuleInterop?: boolean;
     /** @internal */ showConfig?: boolean;
     useDefineForClassFields?: boolean;
+    /** @internal */ tscBuild?: boolean;
 
     [option: string]: CompilerOptionsValue | TsConfigSourceFile | undefined;
 }
